6679cf325fb7637cacd5d7ea980a49f3aae77778
GodRingChan <147848417@qq.com>
2019-08-27 00:41:14 +0800

    1

--- test_python_400/test.py
+++ test_python_400/test.py
@@ -1,6 +1,9 @@
-y = [x*2 for x in range(5) if not x%2 == 0]
-print(y)
+# y = [x*2 for x in range(5) if not x%2 == 0]
+# print(y)
 
+text = "i am %d years old" % 22
+print("i said %s" % text)
+print("i said %r" % text)
 
 # for x in range(100):
 #     if x%7 == 0:
